0ee0f6fdb57eab93fbfcab8c5edf896e3da76b8a
Anilpusa8 <151705781+Anilpusa8@users.noreply.github.com>
2024-01-08 14:18:29 +0530

    Update main.tf

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -3,31 +3,6 @@ resource "azurerm_resource_group" "demo" {
   name     = "demo-resources"
   location = "West Europe"
 }
-
-resource "azurerm_virtual_network" "demo" {
-  name                = "demo-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.demo.location
-  resource_group_name = azurerm_resource_group.demo.name
-}
-
-resource "azurerm_subnet" "demo" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.demo.name
-  virtual_network_name = azurerm_virtual_network.demo.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_network_interface" "demo" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.demo.location
-  resource_group_name = azurerm_resource_group.demo.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.demo.id
-    private_ip_address_allocation = "Dynamic"
-  }
 }
 
 resource "azurerm_linux_virtual_machine" "demo" {

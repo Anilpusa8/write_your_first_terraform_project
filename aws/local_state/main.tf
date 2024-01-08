064e53b22ca7f05282185b46b5aceaf10ec00e36
Anilpusa8 <151705781+Anilpusa8@users.noreply.github.com>
2024-01-08 14:01:36 +0530

    Update main.tf

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -13,14 +13,11 @@ provider "azurerm" {
   region  = "east-us-2"
 }
 
-resource "azurerm_vmware_cluster" "example" {
-  name               = "example-Cluster"
-  vmware_cloud_id    = azurerm_vmware_private_cloud.example.id
+resource "azurerm_vmware_cluster" "Demo" {
+  name               = "Demo"
   cluster_node_count = 3
-  sku_name           = "av36"
 
   tags = {
     Name = "Terraform_Demo"
   }
 }
-
